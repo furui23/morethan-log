8c77e7d0a11f6a67dfa4a423066ee9a0da9c0a52
Agnes <agnes@sapiens.ai>
2026-08-31 11:00:42 +0000

    fix: use correct authToken param for notion-client@6.x

--- src/apis/notion-client/getRecordMap.ts
+++ src/apis/notion-client/getRecordMap.ts
@@ -2,7 +2,7 @@ import { CONFIG } from "site.config"
 import { NotionAPI } from "notion-client"
 
 export const getRecordMap = async (pageId: string) => {
-  const api = new NotionAPI({ notionToken: CONFIG.notionConfig.api })
+  const api = new NotionAPI({ authToken: CONFIG.notionConfig.api })
   const recordMap = await api.getPage(pageId)
   return recordMap
 }

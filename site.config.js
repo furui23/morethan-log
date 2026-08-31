8d4ad12df409ab0bae32b3e9327715b35fb1e3e4
Furious Rush <furui081123@gmail.com>
2026-08-31 17:07:30 +0800

    Update blog title and link in site.config.js

--- site.config.js
+++ site.config.js
@@ -18,13 +18,13 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "fr-blog",
+    description: "welcome to fr blog",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://frblog.netlify.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
